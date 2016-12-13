032b5bcc8eb7f0eff37ef8c3fded765a00f299d9
Luca Tabone <taboneluca3@gmail.com>
2016-12-13 13:07:03 +0100

    Provide session header name to ws module

--- src/apps/ws.js
+++ src/apps/ws.js
@@ -6,6 +6,7 @@ const config = require('../../config')
 const args = [
   '--dbURL', config.db.url,
   '--sessionCookie', config.session.cookie,
+  '--sessionHeader', config.session.header,
   '--sessionMaxAge', config.session.maxAge,
   '--port', config.ws.port,
   '--instances', config.ws.instances
